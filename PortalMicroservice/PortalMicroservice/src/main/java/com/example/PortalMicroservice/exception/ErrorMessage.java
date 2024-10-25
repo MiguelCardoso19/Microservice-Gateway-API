b35a6a5eba16bff0249f5114416e8d34be28bee9
MiguelCardoso <miguel40cardoso@gmail.com>
2024-10-25 16:17:56 +0100

    fixed error code

--- PortalMicroservice/PortalMicroservice/src/main/java/com/example/PortalMicroservice/exception/ErrorMessage.java
+++ PortalMicroservice/PortalMicroservice/src/main/java/com/example/PortalMicroservice/exception/ErrorMessage.java
@@ -3,17 +3,17 @@ package com.example.PortalMicroservice.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorMessage {
-    INVALID_TOKEN("AUTH001", "Invalid token or username mismatch", HttpStatus.UNAUTHORIZED),
-    USER_ID_NOT_FOUND("USR001", "User not found with ID: %s", HttpStatus.NOT_FOUND),
-    USER_NAME_NOT_FOUND("USR002", "User not found with name: %s", HttpStatus.NOT_FOUND),
-    ADDRESS_NOT_FOUND("ADDR001", "Address not found with ID: %s", HttpStatus.NOT_FOUND),
-    USER_OPERATION_ERROR("USR003", "User operation failed due to the following error/s: %s", HttpStatus.CONFLICT),
-    ADDRESS_OPERATION_ERROR("ADDR002", "Address operation failed due to the following error/s: %s", HttpStatus.CONFLICT),
-    INVALID_AUTHORIZATION_HEADER("AUTH002", "Authorization header missing or invalid", HttpStatus.UNAUTHORIZED),
-    INVALID_PASSWORD("AUTH003", "This password is not valid", HttpStatus.UNAUTHORIZED),
-    INVALID_REFRESH_TOKEN("AUTH004", "This refresh token is not valid", HttpStatus.UNAUTHORIZED),
-    GENERIC_MESSAGE("GEN001", "An unexpected error occurred. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR),
-    USER_CREDENTIALS_VALIDATION_ERROR("USR004", "User operation failed due to the following error/s: %s", HttpStatus.CONFLICT);
+    INVALID_TOKEN("INVALID_TOKEN", "Invalid token or username mismatch", HttpStatus.UNAUTHORIZED),
+    USER_ID_NOT_FOUND("USER_ID_NOT_FOUND", "User not found with ID: %s", HttpStatus.NOT_FOUND),
+    USER_NAME_NOT_FOUND("USER_NAME_NOT_FOUND", "User not found with name: %s", HttpStatus.NOT_FOUND),
+    ADDRESS_NOT_FOUND("ADDRESS_NOT_FOUND", "Address not found with ID: %s", HttpStatus.NOT_FOUND),
+    USER_OPERATION_ERROR("USER_OPERATION_ERROR", "User operation failed due to the following error/s: %s", HttpStatus.CONFLICT),
+    ADDRESS_OPERATION_ERROR("ADDRESS_OPERATION_ERROR", "Address operation failed due to the following error/s: %s", HttpStatus.CONFLICT),
+    INVALID_AUTHORIZATION_HEADER("INVALID_AUTHORIZATION_HEADER", "Authorization header missing or invalid", HttpStatus.UNAUTHORIZED),
+    INVALID_PASSWORD("INVALID_PASSWORD", "This password is not valid", HttpStatus.UNAUTHORIZED),
+    INVALID_REFRESH_TOKEN("INVALID_REFRESH_TOKEN", "This refresh token is not valid", HttpStatus.UNAUTHORIZED),
+    GENERIC_MESSAGE("GENERIC_MESSAGE", "An unexpected error occurred. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_CREDENTIALS_VALIDATION_ERROR("USER_CREDENTIALS_VALIDATION_ERROR", "User operation failed due to the following error/s: %s", HttpStatus.CONFLICT);
 
     private final String code;
     private final String message;
